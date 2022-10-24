a10395879dc2802af0bfbd532b00a90350041e14
Brian Pham <brianpham@Brians-Mac-mini.home>
2022-10-24 10:30:58 -0700

    incorporated GraphQL server with express server

--- kensa-ts/server/resolvers.ts
+++ kensa-ts/server/resolvers.ts
@@ -1,3 +1,5 @@
+import { start } from "repl";
+
 export const resolvers = {
   Query: {
     // Get all users
@@ -9,6 +11,7 @@ export const resolvers = {
     },
     // Get a single user by ID
     user: async (_: any, { id }: any, { db }: any, info: any) => {
+      // console.log(req)
       // Getting operation name of query
       // console.log(info.operation.name.value)
       const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
@@ -20,13 +23,23 @@ export const resolvers = {
       return result.rows;
     },
     // Get a single project by ID
-    project: async (_: any, { id }: any, { db }: any, info: any) => {
-      const result = await db.query('SELECT * FROM projects WHERE id = $1', [id]);
+    project: async (parent: any, args: any, { db }: any, info: any) => {
+      // console.log(req.body.query)   // logging out query string sent from frontend
+      // console.log(req.body?.operationName ? req.body.operationName : 'Operation Name is not defined by client')    // logging operation name
+      // console.log(req.headers.token)
+      // console.log(context)
+      // console.log(info.returnType)
+      // console.log('inside project resolver startTime', context.startTime)
+      const result = await db.query('SELECT * FROM projects WHERE id = $1', [args.id]);
+      // const end = Date.now();
+      // console.log('inside project resolver endTime', end)
+      // console.log(end - context.startTime)
       return result.rows[0];
     },
     // Get history log
     historyLog: async (_: any, __: any, { db }: any, info: any) => {
       const result = await db.query('SELECT * FROM history_log;');
+      
       return result.rows;
     }
 
@@ -43,7 +56,11 @@ export const resolvers = {
       return result.rows[0];
     },
     history_log: async ({ id: project_id }: any, __: any, { db }: any, info: any) => {
+      // console.log('inside history_log resolver', startTime)
       const result = await db.query('SELECT * FROM history_log WHERE project_id = $1', [project_id]);
+      // const end = Date.now();
+      // console.log('inside history_log resolver endTime', end)
+      // console.log(end - startTime)
       return result.rows;
     }
   },
--- kensa-ts/server/server.ts
+++ kensa-ts/server/server.ts
@@ -1,43 +1,102 @@
-import { ApolloServer } from "apollo-server";
+// import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPlugin } from "apollo-server-plugin-base";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from 'express'
+import cors from 'cors'
+import http from 'http'
+import bodyParser from 'body-parser'
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
 import db from "./models/db";
+import { GraphQLRequestContext } from "apollo-server-types";
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    // console.log('context from top level')
-    return {
-      db,
-      startTime: Date.now()
-    }
-  },
-})
+async function startApolloServer() {
+  const app = express()
+  const PORT = process.env.PORT || 3000
+  const httpServer = http.createServer(app)
 
-server.listen().then(({url}) => {
-  console.log(`Server runing on ${url}`)
-})
+  // Testing with plugin
+  // const pluginOne = {
+  //   // Fires whenever a GraphQL request is received from a client
+  //   async requestDidStart(requestContext: any) {
+  //     // console.log(requestContext.request.operationName)
+  //     // console.log(`Request started! Query: ${requestContext.request.query}`)
+  //     const start = Date.now()
+  //     let op: string
+  //     return {
+  //       async executionDidStart(executionRequestContext: any) {
+  //         return {
+  //           willResolveField({source, args, contextValue, info}: any) {
+  //             const start = Date.now();
+  //             return (error: any, result: any) => {
+  //               const end = Date.now();
+  //               console.log(`Field ${info.parentType.name}.${info.fieldName} took ${end - start}ms`)
+  //               if (error) {
+  //                 console.log(`It failed with ${error}`)
+  //               } else {
+  //                 console.log(`It returned ${result}`)
+  //               }
+  //             }
+  //           }
+  //         }
+  //       },
+  //       async didResolveOperation(context: any) {
+  //         // console.log(context.contextValue)
+  //         op = context.operationName
+  //       },
+  //       async willSendResponse(context: any) {
+  //         if (op === 'IntrospectionQuery') return;
+  //         // console.log(context.response.body.singleResult) // response data
+  //         // console.log(context) // response data
+  //         // console.log(context.document)
+  //         const stop = Date.now()
+  //         const elapsed = stop - start
+  //         console.log(`operation=${op} duration=${elapsed}ms`)
+  //         // const result = await db.query('SELECT * FROM projects WHERE id = $1', [1])
+  //         // console.log(result.rows)
+  //       }
+  //     }
+  //   }
+  // }
 
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // plugins: [ApolloServerPluginDrainHttpServer({httpServer}), pluginOne],
+  })
 
-// import express, { Request, Response } from 'express'
-// import path from 'path'
+  await apolloServer.start();
 
-// const app = express()
-// const PORT = process.env.PORT || 3000
+  // Express REST API route
+  app.get('/login', (req, res) => {
+    console.log('in login route')
+    res.status(200).json({ username: 'brian', isLoggedIn: true })
+  })
 
-// app.use('/', express.static(path.join(__dirname, '../dist')))
+  app.get('/hello', (req, res) => {
+    res.status(200).send('Hello from express')
+  })
+  
+  // GraphQL logic
+  // Testing with plugin, need to refactor later
+  app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(apolloServer, {
+    context: async ({req, res}: any) => {
+      if (req.body.operationName === 'IntrospectionQuery') return;
+      console.log(req.body.operationName);
+      // const user = await db.query('SELECT * FROM users WHERE id = $1', [1])
+      return {
+        req,
+        res,
+        db,
+        // startTime: Date.now(),
+      }
+    },
+  }))
+  
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-// app.get('/age', (req: Request, res: Response): void => {
-//   const age: number = 39;
-//   res.json({ message: `I am ${age} years old` })
-// })
-
-// app.get('/hello', (req: Request, res: Response) => {
-//   return res.status(200).json({ message: 'Hello! How are you?' })
-// })
-
-// app.listen(PORT, (): void => {
-//   console.log(`Server listening on port: ${PORT}`)
-// })
+}
+startApolloServer();
 
