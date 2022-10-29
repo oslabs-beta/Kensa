fbf271a1dde628b80cde7883fc41c33d2692f68f
Brian Pham <brianpham@Brians-Mac-mini.home>
2022-10-29 11:07:02 -0700

    create OperationTable and ChartMetrics components

--- kensa-ts/src/components/Dashboard.tsx
+++ kensa-ts/src/components/Dashboard.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Flex, Box, Container, Divider, Stack, Avatar, Input, InputGroup, InputLeftElement, Icon } from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
+import OperationTable from './OperationTable';
+import ChartMetrics from './ChartMetrics';
 
 const Dashboard = () => {
+  const [showMetrics, setShowMetrics] = useState(false);
+
   return (
-    <Flex flex='6' bgColor='#f9fafc' direction='column'>
-      <Stack direction='row' spacing={3} bgColor='white' h='7%' w='100%' align='center' p='20px'>
+    <Flex flex='5' bgColor='#f9fafc' direction='column'>
+      <Stack direction='row' spacing={3} bgColor='white' h='8%' w='100%' align='center' p='20px'>
         <InputGroup>
           <InputLeftElement 
             pointerEvents='none'
@@ -16,10 +20,13 @@ const Dashboard = () => {
         </InputGroup>
         <Avatar name='Kensa' size='md' justifyItems='flex-end' />
       </Stack>
+
       <Divider />
-      <Container>
-        Stats
-      </Container>
+
+      <Flex h='100%'>
+        <OperationTable setShowMetrics={setShowMetrics} showMetrics={showMetrics} />
+        {showMetrics && <ChartMetrics />}
+      </Flex>
     </Flex>
   );
 };
