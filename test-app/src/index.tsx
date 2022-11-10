2fd9ec6e3561308633336f64c0ff78c9a6767275
brianhip <brian7terra@gmail.com>
2022-11-09 20:20:44 -0700

    kensa app only

--- test-app/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import './assets/styles/styles.scss';
-
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root'));
