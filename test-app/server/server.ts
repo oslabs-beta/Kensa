6a31187a51ce3add43c5f009b0aa3cedaae49375
Brian Pham <br.pham13@gmail.com>
2023-04-15 14:09:15 -0700

    replace new api key for test app

--- test-app/server/server.ts
+++ test-app/server/server.ts
@@ -1,35 +1,39 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { typeDefs } from "./schema";
-import { resolvers } from "./resolvers";
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
 import testDb from './models/testDb';
-import { kensaPlugin, getContext} from 'kensats';
+import { kensaPlugin, getContext } from 'kensats';
 
 async function startApolloServer() {
   const app = express();
   const PORT = process.env.PORT || 3000;
-  // Start new apollo server 
+  // Start new apollo server
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [kensaPlugin]
-  }); 
+    plugins: [kensaPlugin],
+  });
 
   await apolloServer.start();
 
-  const api = '65b00bbe-3b3b-4fea-8d3f-de3a84338914';
-  
-  app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(apolloServer, {
-    // when referencing the database in resolvers, use 'db' when deconstructing context object
-    context: async ({req, res}: any) => (await getContext({req, res}, api, testDb))
-  }));
-  
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  const api = 'f1d275e5-5557-4aa0-bb29-ebd0ab871cf2';
 
+  app.use(
+    '/graphql',
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(apolloServer, {
+      // when referencing the database in resolvers, use 'db' when deconstructing context object
+      context: async ({ req, res }: any) =>
+        await getContext({ req, res }, api, testDb),
+    })
+  );
+
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
 
 startApolloServer();
-
